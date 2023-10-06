2f4d7de7a60b4bd0ca0110129d1197c16cdcac7a
LCarluen <logancarluen@icloud.com>
2023-10-06 19:50:15 +0100

    change car controller script

--- F1 Simulator/Assets/Car controller.cs
+++ F1 Simulator/Assets/Car controller.cs
@@ -14,41 +14,35 @@ public class WheelController : MonoBehaviour {
    [SerializeField] Transform backRightTransform;
    [SerializeField] Transform backLeftTransform;
 
-   public float acceleration = 500f;
-   public float brakingForce = 300f;
-   public float maxTurnAngle = 15f;
+   public float acceleration = 0f;
+   public float brakingForce = 0f;
+   public float maxTurnAngle = 0f;
 
    private float currentAcceleration = 0f;
    private float currentBrakeForce = 0f;
    private float currentTurnAngle = 0f;
 
    private void FixedUpdate() {
-      // Get forward/reverse acceleration from the vertical axis (W and S keys)
       float accelerationInput = Input.GetAxis("Vertical");
       currentAcceleration = accelerationInput * acceleration;
 
-      // Apply braking force if the spacebar is pressed
-      if (Input.GetKey(KeyCode.Space))
+      if (Input.GetKey(KeyCode.S))
           currentBrakeForce = brakingForce;
       else
           currentBrakeForce = 0f;
 
-      // Apply acceleration to back wheels
       backRight.motorTorque = currentAcceleration;
       backLeft.motorTorque = currentAcceleration;
 
-      // Apply braking force to all wheels
       frontRight.brakeTorque = currentBrakeForce;
       frontLeft.brakeTorque = currentBrakeForce;
       backRight.brakeTorque = currentBrakeForce;
       backLeft.brakeTorque = currentBrakeForce;
 
-      // Steering
       currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
       frontLeft.steerAngle = currentTurnAngle;
       frontRight.steerAngle = currentTurnAngle;
 
-      // Update wheel meshes
       UpdateWheel(frontLeft, frontLeftTransform);
       UpdateWheel(frontRight, frontRightTransform);
       UpdateWheel(backLeft, backLeftTransform);
@@ -57,12 +51,10 @@ public class WheelController : MonoBehaviour {
 
    void UpdateWheel(WheelCollider col, Transform trans) {
 
-     // Get wheel collider state
      Vector3 position;
      Quaternion rotation;
      col.GetWorldPose(out position, out rotation);
 
-     // Set wheel transform state
      trans.position = position;
      trans.rotation = rotation;
    }
