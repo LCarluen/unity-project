e7506a2dcc60ecb252e08afc425cd3352a12506a
LCarluen <logancarluen@icloud.com>
2023-10-12 09:45:37 +0100

    Added working speedometer

---
+++ F1 Simulator/Assets/Speedometer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Speedometer : MonoBehaviour
+{
+    public Rigidbody target;
+
+    public float maxSpeed = 0.0f; // The maximum speed of the target ** IN KM/H **
+
+    public float minSpeedArrowAngle;
+    public float maxSpeedArrowAngle;
+
+    [Header("UI")]
+    public Text speedValue;
+    public RectTransform needle;
+
+    private float speed = 0.0f;
+    private void Update()
+    {
+        // 3.6f to convert in kilometers
+        // ** The speed must be clamped by the car controller **
+        speed = target.velocity.magnitude * 3.6f;
+
+        if (speedValue != null)
+            speedValue.text = ((int)speed) + " km/h";
+        if (needle != null)
+            needle.localEulerAngles =
+                new Vector3(0, 0, Mathf.Lerp(minSpeedArrowAngle, maxSpeedArrowAngle, speed / maxSpeed));
+    }
+}
