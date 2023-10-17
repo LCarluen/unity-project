ba3d42147d89c254912b0c2ebc268aa047d5b26a
LCarluen <logancarluen@icloud.com>
2023-10-17 19:38:23 +0100

    Update TireDeterioration.cs

--- F1 Simulator/Assets/TireDeterioration.cs
+++ F1 Simulator/Assets/TireDeterioration.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TireDeterioration : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class TireDeterioration : MonoBehaviour {
+
+    [SerializeField] WheelCollider frontRight;
+    [SerializeField] WheelCollider frontLeft;
+    [SerializeField] WheelCollider backRight;
+    [SerializeField] WheelCollider backLeft;
+
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
