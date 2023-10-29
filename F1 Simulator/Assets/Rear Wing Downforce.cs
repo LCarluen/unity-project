93861f47f8d88fcecc11cfa182a3b9baa642d5f7
LCarluen <logancarluen@icloud.com>
2023-10-29 22:04:13 +0000

    creating rear wing force

---
+++ F1 Simulator/Assets/Rear Wing Downforce.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RearWingDownforce : MonoBehaviour
+{
+
+    [SerializeField] Rigidbody car;
+
+
+
+    void Update()
+    {
+
+
+    }
+
+}
