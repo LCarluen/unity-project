d084e30a1205e828082af61a9040680a932368b7
LCarluen <logancarluen@icloud.com>
2023-11-20 20:34:36 +0000

    tire det

--- F1 Simulator/Assets/Tire deterioration.cs
+++ F1 Simulator/Assets/Tire deterioration.cs
@@ -6,47 +6,13 @@ public class TireDeterioration : MonoBehaviour
     [SerializeField] WheelCollider frontLeft;
     [SerializeField] WheelCollider backRight;
     [SerializeField] WheelCollider backLeft;
-    public float initialTireHealth = 100f; // Initial tire health
-    public float tireDeteriorationRate = 0.1f;
+    public float initialTireHealth = 100f;
+    public float tireDeteriorationRate = 20f;
     public float maxDeterioration = 100f;
-    public WheelFrictionCurve sidewaysFriction;
+    private float colliderCircumference = (float)(3.14 * 0.5 * 2);
 
-    private float currentTireHealth;
 
-    void Start()
-    {
-        currentTireHealth = initialTireHealth;
-    }
-
-    void Update()
-    {
-        float distanceTraveled = Vector3.Distance(transform.position, transform.position + transform.forward);
-        DeteriorateTires(distanceTraveled * tireDeteriorationRate);
-
-        // Check for tire blowout or replacement condition
-        if (currentTireHealth <= 66f)
-        {
-            sidewaysFriction.extremumValue =
-        }
-        else if (currentTireHealth >= maxDeterioration)
-        {
-            ReplaceTire();
-        }
-    }
-
-    void DeteriorateTires(float amount)
-    {
-        // Deteriorate the tires by the specified amount
-        currentTireHealth -= amount;
-
-        // Clamp the tire health to stay within valid range
-        currentTireHealth = Mathf.Clamp(currentTireHealth, 0f, maxDeterioration);
-    }
-
-    private void FixedUpdate()
-    {
-
-    }
-    }
-
+    
 }
+
+
