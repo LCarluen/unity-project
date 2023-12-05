35467061e654eb95ba1cbc34781daddefe5ed39d
LCarluen <logancarluen@icloud.com>
2023-12-05 11:40:49 +0000

    fully working tire deter

--- F1 Simulator/Assets/Tire deterioration.cs
+++ F1 Simulator/Assets/Tire deterioration.cs
@@ -6,88 +6,111 @@ public class TireDeterioration : MonoBehaviour
     [SerializeField] WheelCollider frontLeft;
     [SerializeField] WheelCollider backRight;
     [SerializeField] WheelCollider backLeft;
-    public float initialTireHealth = 100f;
+    public float initialTireHealth = 228800f;
     public float tireDeteriorationRate = 20f;
     public float maxDeterioration = 100f;
     public float colliderCircumference = 3.14f * 0.5f * 2f;
+    public float wear = 0f;
 
     public float distanceTravelled = 0.0f;
 
-void FixedUpdate()
-{
-    float angularVelocity = backLeft.rpm * colliderCircumference / 60.0f;
-    distanceTravelled += angularVelocity * 0.5f * Time.deltaTime;
-
-    if((distanceTravelled / initialTireHealth) >= 0.8f)
+    void FixedUpdate()
     {
+        float angularVelocity = backLeft.rpm * colliderCircumference / 60.0f;
+        distanceTravelled += angularVelocity * 0.5f * Time.deltaTime;
+        wear = (distanceTravelled / initialTireHealth) / 1000f;
+
         WheelFrictionCurve WFC;
-        WFC = frontRight.sidewaysFriction;
-        WFC = frontLeft.sidewaysFriction;
-        WFC = backRight.sidewaysFriction;
-        WFC = backLeft.sidewaysFriction;
-        WFC.extremumValue = 2.5f;
-        frontRight.sidewaysFriction = WFC;
-        frontLeft.sidewaysFriction = WFC;
-        backRight.sidewaysFriction = WFC;
-        backLeft.sidewaysFriction = WFC;
+
+        if (wear <= 0.2f)
+        {
+            WFC = frontRight.sidewaysFriction;
+            WFC.extremumValue = 3.3f;
+            frontRight.sidewaysFriction = WFC;
+
+            WFC = frontLeft.sidewaysFriction;
+            WFC.extremumValue = 3.3f;
+            frontLeft.sidewaysFriction = WFC;
+
+            WFC = backRight.sidewaysFriction;
+            WFC.extremumValue = 3.3f;
+            backRight.sidewaysFriction = WFC;
+
+            WFC = backLeft.sidewaysFriction;
+            WFC.extremumValue = 3.3f;
+            backLeft.sidewaysFriction = WFC;
+        }
+        else if (wear <= 0.4f && wear > 0.2f)
+        {
+            WFC = frontRight.sidewaysFriction;
+            WFC.extremumValue = 3.1f;
+            frontRight.sidewaysFriction = WFC;
+
+            WFC = frontLeft.sidewaysFriction;
+            WFC.extremumValue = 3.1f;
+            frontLeft.sidewaysFriction = WFC;
+
+            WFC = backRight.sidewaysFriction;
+            WFC.extremumValue = 3.1f;
+            backRight.sidewaysFriction = WFC;
+
+            WFC = backLeft.sidewaysFriction;
+            WFC.extremumValue = 3.1f;
+            backLeft.sidewaysFriction = WFC;
+        }
+        else if (wear <= 0.6f && wear > 0.4f)
+        {
+            WFC = frontRight.sidewaysFriction;
+            WFC.extremumValue = 2.9f;
+            frontRight.sidewaysFriction = WFC;
+
+            WFC = frontLeft.sidewaysFriction;
+            WFC.extremumValue = 2.9f;
+            frontLeft.sidewaysFriction = WFC;
+
+            WFC = backRight.sidewaysFriction;
+            WFC.extremumValue = 2.9f;
+            backRight.sidewaysFriction = WFC;
+
+            WFC = backLeft.sidewaysFriction;
+            WFC.extremumValue = 2.9f;
+            backLeft.sidewaysFriction = WFC;
+        }
+        else if (wear <= 0.8f && wear > 0.6f)
+        {
+            WFC = frontRight.sidewaysFriction;
+            WFC.extremumValue = 2.7f;
+            frontRight.sidewaysFriction = WFC;
+
+            WFC = frontLeft.sidewaysFriction;
+            WFC.extremumValue = 2.7f;
+            frontLeft.sidewaysFriction = WFC;
+
+            WFC = backRight.sidewaysFriction;
+            WFC.extremumValue = 2.7f;
+            backRight.sidewaysFriction = WFC;
+
+            WFC = backLeft.sidewaysFriction;
+            WFC.extremumValue = 2.7f;
+            backLeft.sidewaysFriction = WFC;
+        }
+        else if (wear <= 1f && wear > 0.8f)
+        {
+            WFC = frontRight.sidewaysFriction;
+            WFC.extremumValue = 2.5f;
+            frontRight.sidewaysFriction = WFC;
+
+            WFC = frontLeft.sidewaysFriction;
+            WFC.extremumValue = 2.5f;
+            frontLeft.sidewaysFriction = WFC;
+
+            WFC = backRight.sidewaysFriction;
+            WFC.extremumValue = 2.5f;
+            backRight.sidewaysFriction = WFC;
+
+            WFC = backLeft.sidewaysFriction;
+            WFC.extremumValue = 2.5f;
+            backLeft.sidewaysFriction = WFC;
+        }
     }
-
-    if((distanceTravelled / initialTireHealth) >= 0.6f && (distanceTravelled / initialTireHealth) < 0.8f)
-    {
-        WheelFrictionCurve WFC;
-        WFC = frontRight.sidewaysFriction;
-        WFC = frontLeft.sidewaysFriction;
-        WFC = backRight.sidewaysFriction;
-        WFC = backLeft.sidewaysFriction;
-        WFC.extremumValue = 2.7f;
-        frontRight.sidewaysFriction = WFC;
-        frontLeft.sidewaysFriction = WFC;
-        backRight.sidewaysFriction = WFC;
-        backLeft.sidewaysFriction = WFC;
-    }
-
-    if((distanceTravelled / initialTireHealth) >= 0.4f && (distanceTravelled / initialTireHealth) < 0.6f)
-    {
-        WheelFrictionCurve WFC;
-        WFC = frontRight.sidewaysFriction;
-        WFC = frontLeft.sidewaysFriction;
-        WFC = backRight.sidewaysFriction;
-        WFC = backLeft.sidewaysFriction;
-        WFC.extremumValue = 2.9f;
-        frontRight.sidewaysFriction = WFC;
-        frontLeft.sidewaysFriction = WFC;
-        backRight.sidewaysFriction = WFC;
-        backLeft.sidewaysFriction = WFC;
-    }
-
-    if((distanceTravelled / initialTireHealth) >= 0.2 && (distanceTravelled / initialTireHealth) < 0.4)
-    {
-        WheelFrictionCurve WFC;
-        WFC = frontRight.sidewaysFriction;
-        WFC = frontLeft.sidewaysFriction;
-        WFC = backRight.sidewaysFriction;
-        WFC = backLeft.sidewaysFriction;
-        WFC.extremumValue = 3.1f;
-        frontRight.sidewaysFriction = WFC;
-        frontLeft.sidewaysFriction = WFC;
-        backRight.sidewaysFriction = WFC;
-        backLeft.sidewaysFriction = WFC;
-    }
-
-    if((distanceTravelled / initialTireHealth) < 0.2f)
-    {
-        WheelFrictionCurve WFC;
-        WFC = frontRight.sidewaysFriction;
-        WFC = frontLeft.sidewaysFriction;
-        WFC = backRight.sidewaysFriction;
-        WFC = backLeft.sidewaysFriction;
-        WFC.extremumValue = 3.3f;
-        frontRight.sidewaysFriction = WFC;
-        frontLeft.sidewaysFriction = WFC;
-        backRight.sidewaysFriction = WFC;
-        backLeft.sidewaysFriction = WFC;
-    }
-
-  }
-
 }
